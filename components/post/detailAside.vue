c5a374175a132184b51c65edb3e10f846e2ed04b
853187348@qq.com <853187348@qq.com>
2019-07-21 21:08:21 +0800

    冯华辉上传攻略详情页静态页面-除评论其他模块完成

---
+++ components/post/detailAside.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="aside">
+    <h3>相关攻略</h3>
+    <!-- 列表 -->
+    <el-row type="flex" class="list">
+      <div>
+        <img src="../../assets/images/1.jpg" >
+      </div>
+      <div>
+        <div>我是标题</div>
+        <span>2019-01-01 12:00 阅读:2</span>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style lang="less" scoped>
+.aside{
+  margin-left: 20px;
+  h3{
+    border-bottom: 1px solid #ccc;
+    padding: 20px 0;
+  }
+  .list{
+     padding: 20px 0;
+     border-bottom: 1px solid #ccc;
+     img{
+       width: 80px;
+       height: 80px;
+       margin-right: 10px;
+     }
+     >div{
+       height: 80px;
+       &:nth-child(2){
+       div{
+         font-size: 18px;
+       }
+       span{
+         color: #ccc;
+         align-content: bottom;
+       }
+     }
+     }
+     
+  }
+ }
+</style>
+
---
+++ components/post/detailComment.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="commemt">
+    <h3>评论</h3>
+    <!-- 文本框 -->
+    <el-input
+     class="textarea" 
+     type="textarea" 
+     :rows="2" 
+     placeholder="请输入内容" 
+     v-model="textarea"
+     style="margin-bottom:20px;"
+     ></el-input>
+     <!-- 提交按钮 -->
+     <el-button type="primary" style="margin-bottom:20px;float:right;">提交</el-button>
+    <!-- 图片上传 -->
+    <el-upload
+      action="https://jsonplaceholder.typicode.com/posts/"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove"
+      class="upload"
+      style="margin-bottom:20px;"
+    >
+      <i class="el-icon-plus"></i>
+    </el-upload>
+    <el-dialog :visible.sync="dialogVisible" size="tiny">
+      <img width="100%" :src="dialogImageUrl" alt />
+    </el-dialog>
+    <!-- 评价列表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textarea: "", //文本域
+      dialogVisible:false, //
+      dialogImageUrl:'' //图片路径
+    };
+  },
+  methods:{
+    // 点击已上传的文件链接时的钩子
+    handlePictureCardPreview(file){
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    // 文件列表移除文件时的钩子
+    handleRemove(file, fileList){}
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.commemt {
+  h3 {
+    font-weight: normal;
+    padding-bottom: 20px;
+  }
+}
+</style>
+
